5413d959c1430704b2a636baf1c2703c55401b8d
Vibhav Singamshetty <vibsingamshetty@gmail.com>
2026-03-13 13:07:23 -0700

    Cleanliness

--- plugins/install-scripts/zvm.sh
+++ plugins/install-scripts/zvm.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 PLUGINS_BIN="$HOME/Developer/Personal/setup/plugins/bin"
-ZVM_SRC="$HOME/Developer/Personal/zvm"
 
 go build -C "$HOME/Developer/Personal/zvm" -ldflags "-s -w" -o "$PLUGINS_BIN/"
 

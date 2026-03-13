8e901d42909a4b08ee780437b6d86f7c45200e6e
Vibhav Singamshetty <vibsingamshetty@gmail.com>
2026-03-13 12:05:44 -0700

    Fix scripts directory specification

--- scripts/install-plugins.sh
+++ scripts/install-plugins.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-PLUGINS_INSTALL_SCRIPTS_DIR=./plugins/install-scripts
+PLUGINS_INSTALL_SCRIPTS_DIR="$HOME/Developer/Personal/setup/plugins/install-scripts"
 
 for file in $PLUGINS_INSTALL_SCRIPTS_DIR/*.sh; do
     echo "Running plugin install script: $file"
